e9a2bca61af4ec6917eaf71a595271a190e8f3de
Eugenio Garcia <eugenio5039@gmail.com>
2024-12-30 22:36:21 -0600

    Add - recipe page with titles

---
+++ src/app/lib/data.ts
@@ -0,0 +1,13 @@
+const SERVER_URL = "http://localhost:3300";
+
+export async function fetchAllRecipes(accessToken: string) {
+  const response = await fetch(SERVER_URL + "/api/v1/recipes/index", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+  });
+  const result = await response.json();
+  return result;
+}
