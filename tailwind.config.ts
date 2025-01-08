94b7933a042ccf54e51718ca4c7bc836faf32478
Eugenio Garcia <eugenio5039@gmail.com>
2025-01-07 21:33:20 -0600

    Add - Colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
 
 export default {
   content: [
@@ -9,8 +10,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        ...colors,
+        primary: {
+          "50": "#fcf4f4",
+          "100": "#f9e7e7",
+          "200": "#f5d3d3",
+          "300": "#eebbbb",
+          "400": "#e08989",
+          "500": "#d06363",
+          "600": "#bb4747",
+          "700": "#9d3838",
+          "800": "#823232",
+          "900": "#6d2f2f",
+          "950": "#3a1515",
+        },
       },
     },
   },
