57415acaf2edbce9edf3921e2bf9761aad5804b7
Eugenio Garcia <eugenio5039@gmail.com>
2025-03-31 20:41:07 -0600

    Fix - lint errors cookbook create page

--- src/app/dashboard/cookbook/create/page.tsx
+++ src/app/dashboard/cookbook/create/page.tsx
@@ -1,6 +1,3 @@
-import { fetchCookbooks } from "@/app/lib/data";
-import { Cookbook } from "../../../types/types";
-import { getAccessToken } from "@auth0/nextjs-auth0";
 import Form from "@/app/ui/cookbook/create-form";
 import { Metadata } from "next";
 
@@ -8,11 +5,7 @@ export const metadata: Metadata = {
   title: "Create",
 };
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
+export default async function Page() {
   return (
     <main>
       <h1 className="text-lg">Create Cookbook</h1>
